c1ddcea6c023e89dd7da65c75859bc6a845bc846
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-06-24 11:40:41 +0800

    Sort button

--- CeMeal/View/IngredientView/IngredientView.swift
+++ CeMeal/View/IngredientView/IngredientView.swift
@@ -11,11 +11,35 @@ struct IngredientView: View {
     
     @ObservedObject private var ingredientViewModel = IngredientViewModel()
     @State private var searchQuery = ""
+    @State private var sort: Int = 0
     
     var body: some View {
         NavigationView {
             
             List {
+                    
+                // My ingredients title
+                HStack {
+                    Text("My Ingredients")
+                        .foregroundColor(Color.ui.title)
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                    Menu {
+                        Picker(selection: $sort, label: Text("Sorting options")) {
+                            Text("Title").tag(0)
+                            Text("Buy Date").tag(1)
+                        }
+                    }
+                    label: {
+                        Image(systemName: "arrow.up.arrow.down")
+                            .foregroundColor(.accentColor)
+                    }
+                }
+                .listRowBackground(Color(UIColor.systemGray6))
+                
+                // Filled ingredient list
                 ForEach(ingredientViewModel.ingredients.filter({ searchQuery.isEmpty ? true : $0.title.contains(searchQuery) })) { ingredient in
                     NavigationLink {
                         Text("Chicken Breast")
@@ -23,11 +47,13 @@ struct IngredientView: View {
                         IngredientListView(ingredient: ingredient)
                     }
                 }
+                .listRowBackground(Color(UIColor.systemGray6))
+                
             }
             .listStyle(.plain)
             
             .navigationTitle("Hi, Chef!")
-            .navigationBarColor(backgroundColor: .white, titleColor: UIColor(Color.ui.title))
+            .navigationBarColor(backgroundColor: .systemBackground, titleColor: UIColor(Color.ui.title))
             .toolbar {
                 ToolbarItem(placement: .automatic) {
                     Button {
