ca6d5b4b324a901fccf27c7410bdc5d36c1ee98a
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-06-30 21:52:38 +0800

    Ingredient detail view

--- CeMeal/View/IngredientView/IngredientDetailView.swift
+++ CeMeal/View/IngredientView/IngredientDetailView.swift
@@ -11,13 +11,63 @@ struct IngredientDetailView: View {
     
 //    @Environment(\.isAddIngredientPresented) var isAddIngredientPresented
     @Environment(\.presentationMode) var detailSheet
+    @State private var checkedStorage = 0
+    @State private var buyDate = Date()
+    @State private var expireDate = Date()
+    @State var isReminded = false
     
     var ingredient: Ingredient
+    var storages = ["Cupboard", "Fridge", "Freezer"]
     
     var body: some View {
         NavigationView {
-            Text(ingredient.title)
-        
+            List() {
+                // MARK: Category
+                HStack {
+                    Text("Category")
+                    Spacer()
+                    Text(ingredient.category)
+                        .foregroundColor(.gray)
+                }
+                
+                // MARK: Stored
+                Text("Stored At")
+                ForEach(Array(storages.enumerated()), id: \.offset) { index, storage in
+                    HStack {
+                        Image(systemName: checkedStorage == index ? "checkmark.square.fill" : "square")
+                            .onTapGesture {
+                                checkedStorage = index
+                            }
+                            .foregroundColor(.accentColor)
+                            .padding()
+                        
+                        VStack(alignment: .leading) {
+                            Text(storage.uppercased())
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color.ui.title)
+                            
+                            Text("1-3 months")
+                                .foregroundColor(.accentColor)
+                                .font(
+                                    .system(.callout, design: .serif)
+                                    .italic()
+                                )
+                        }
+                    }
+                }
+                
+                // MARK: Buy date
+                DatePicker("Buy Date", selection: $buyDate, displayedComponents: [.date])
+                
+                // MARK: Expire date
+                DatePicker("Expire Date", selection: $expireDate, displayedComponents: [.date])
+                    .colorMultiply(.red)
+                
+                // MARK: Remind me
+                Toggle("Remind Me", isOn: $isReminded)
+            }
+            .listStyle(.plain)
+            
             .navigationTitle(ingredient.title)
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
@@ -28,15 +78,14 @@ struct IngredientDetailView: View {
                         Text("Cancel")
                             .foregroundColor(.red)
                     }
-
                 }
+                
                 ToolbarItem(placement: .confirmationAction) {
                     Button {
-                        
+                        detailSheet.wrappedValue.dismiss()
                     } label: {
                         Text("Save")
                     }
-
                 }
             }
         }
@@ -45,8 +94,6 @@ struct IngredientDetailView: View {
 
 struct IngredientDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            IngredientDetailView(ingredient: Ingredient(id: "0", title: "Empty", category: "Unknown", expireDay: [1], shelfLife: ["a": "b"]))
-        }
+        IngredientDetailView(ingredient: Ingredient(id: "0", title: "Empty", category: "Unknown", expireDay: [1], shelfLife: ["a": "b"]))
     }
 }
