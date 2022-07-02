3a2b084367c8737b2bea6f176ded60b65f932d76
anantus <maheswaraananta@gmail.com>
2022-07-03 01:46:20 +0700

    integrate recipe view model to recipe

--- CeMeal/Model/Recipe.swift
+++ CeMeal/Model/Recipe.swift
@@ -7,18 +7,49 @@
 
 import Foundation
 
-struct Recipe: Identifiable {
-    
-    var id: String
-    var title: String
-    var thumbnailImage: String?
-    var isFavorite: Bool
-    
-    init(id: String = UUID().uuidString, title: String, thumbnailImage: String? = nil, isFavorite: Bool = false) {
-        self.id = id
-        self.title = title
-        self.thumbnailImage = thumbnailImage
-        self.isFavorite = isFavorite
+struct Recipe : Identifiable {
+    var mealName : String = ""
+    var category : String = ""
+    var area : String = ""
+    var instructions : String = ""
+    var thumbnail : String = ""
+    var youtubeLink : String = ""
+    var ingredients : [String] = []
+    var measurements : [String] = []
+    var source : String = ""
+    var id = UUID()
+
+    init(raw: [String]) {
+        mealName = raw[1]
+        category = raw[3]
+        area = raw[4]
+        instructions = raw[5]
+        thumbnail = raw[6]
+        youtubeLink = raw[8]
+        source = raw[49]
+
+        for ingr_measure_num in 1...20 {
+            let ingredients_col : Int = 8 + ingr_measure_num
+            let recipe_col : Int = 28 + ingr_measure_num
+
+            ingredients.append(raw[ingredients_col])
+            measurements.append(raw[recipe_col])
+        }
     }
-    
 }
+
+//struct Recipe: Identifiable {
+//
+//    var id: String
+//    var title: String
+//    var thumbnailImage: String?
+//    var isFavorite: Bool
+//
+//    init(id: String = UUID().uuidString, title: String, thumbnailImage: String? = nil, isFavorite: Bool = false) {
+//        self.id = id
+//        self.title = title
+//        self.thumbnailImage = thumbnailImage
+//        self.isFavorite = isFavorite
+//    }
+//
+//}
--- CeMeal/View/LeftoverView/LeftoverListView.swift
+++ CeMeal/View/LeftoverView/LeftoverListView.swift
@@ -10,74 +10,70 @@ import SwiftUI
 struct LeftoverListView: View {
     
     @Environment(\.managedObjectContext) private var viewContent
-    @EnvironmentObject var leftoversViewModel:LeftoverViewModel
+    @EnvironmentObject var leftoversViewModel:LeftoversViewModel
     @ObservedObject var leftover:Leftovers
     
     var body: some View {
-        HStack {
-            // Checkbox
-            Image(systemName: leftover.isChecked ? "checkmark.square.fill" : "square")
-                .resizable()
-                .frame(width: 20, height: 20)
-                .onTapGesture {
-                    withAnimation(.easeOut) {
-//                        leftoversViewModel.leftoverIsChecked(leftover: leftover)
-                        //toggle leftover
-                        leftover.isChecked.toggle()
+        if leftover.ingredients != nil{
+            HStack {
+                // Checkbox
+                Image(systemName: leftover.isChecked ? "checkmark.square.fill" : "square")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                    .onTapGesture {
+                        withAnimation(.easeOut) {
+                            //                        leftoversViewModel.leftoverIsChecked(leftover: leftover)
+                            //toggle leftover
+                            leftover.isChecked.toggle()
+                        }
                     }
-                }
-                .foregroundColor(.accentColor)
-            
-            // Content
-            VStack(alignment: .leading) {
-                
-                // Title
-                Text(leftover.ingredients!)
-                    .font(
-                        .title3
-                        .weight(.semibold)
-                    )
-                    .foregroundColor(Color.ui.title)
-                    .textCase(.uppercase)
-                
-                // Category
-                Text(leftover.category ?? "no category")
                     .foregroundColor(.accentColor)
-                    .font(
-                        .system(.callout, design: .serif)
-                        .italic()
-                    )
                 
-                HStack {
-                    Image(systemName: "calendar")
-                        .foregroundColor(.accentColor)
-                        .font(.subheadline)
-
-                    Text(dateToString(leftover.dateCreated!))
-                        .foregroundColor(.gray)
-                        .font(.subheadline)
+                // Content
+                VStack(alignment: .leading) {
                     
-                    Image(systemName: "plus.square.fill.on.square.fill")
-                        .foregroundColor(Color.ui.accent2)
-                        .font(.subheadline)
-
-                    Text(dateToString(leftover.dateExpired!))
-                        .foregroundColor(.gray)
-                        .font(.subheadline)
+                    // Title
+                    Text(leftover.ingredients!)
+                        .font(
+                            .title3
+                                .weight(.semibold)
+                        )
+                        .foregroundColor(Color.ui.title)
+                        .textCase(.uppercase)
+                    
+                    // Category
+                    Text(leftover.category ?? "no category")
+                        .foregroundColor(.accentColor)
+                        .font(
+                            .system(.callout, design: .serif)
+                            .italic()
+                        )
+                    
+                    HStack {
+                        Image(systemName: "calendar")
+                            .foregroundColor(.accentColor)
+                            .font(.subheadline)
+                        
+                        Text(dateToString(leftover.dateCreated!))
+                            .foregroundColor(.gray)
+                            .font(.subheadline)
+                        
+                        Image(systemName: "plus.square.fill.on.square.fill")
+                            .foregroundColor(Color.ui.accent2)
+                            .font(.subheadline)
+                        
+                        Text(dateToString(leftover.dateExpired!))
+                            .foregroundColor(.gray)
+                            .font(.subheadline)
+                    }
+                    .padding(.top, 1)
+                    
                 }
-                .padding(.top, 1)
+                .padding(.horizontal, 16)
                 
             }
-            .padding(.horizontal, 16)
-            
+            .padding(.horizontal)
+            .padding(.vertical, 10)
         }
-        .padding(.horizontal)
-        .padding(.vertical, 10)
     }
 }
-
-//struct IngredientListView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        LeftoverListView(leftover: Leftover(id: "123", title: "Chicken Breast", category: "Poultry", buyDate: Date(), expireDate: Date(), storage: "Fridge", isChecked: true))
-//    }
-//}
