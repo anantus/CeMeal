60fbe8f036190b908a562682d4b2bc857029cd6f
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-07-03 21:37:04 +0800

    Onboarding & main view

--- CeMeal/View/ContentView.swift
+++ CeMeal/View/ContentView.swift
@@ -9,23 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var selection: Int = 0
+    @State var goToTabbedView: Bool = UserDefaults.standard.bool(forKey: "opened_once")
     
     var body: some View {
-        TabView(selection: $selection) {
-            LeftoverView()
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("Ingredient")
-                }
-                .tag(0)
-            
-            FavoriteView()
-                .tabItem {
-                    Image(systemName: "heart")
-                    Text("Favorites")
-                }
-                .tag(1)
+        if goToTabbedView {
+            TabbedView()
+        } else {
+            OnboardingView(goToTabbedView: $goToTabbedView)
         }
     }
     
---
+++ CeMeal/View/TabbedView.swift
@@ -0,0 +1,38 @@
+//
+//  TabbedView.swift
+//  CeMeal
+//
+//  Created by Darma Wiryanata on 03/07/22.
+//
+
+import SwiftUI
+
+struct TabbedView: View {
+    
+    @State private var selection: Int = 0
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            LeftoverView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Ingredient")
+                }
+                .tag(0)
+            
+            FavoriteView()
+                .tabItem {
+                    Image(systemName: "heart")
+                    Text("Favorites")
+                }
+                .tag(1)
+        }
+    }
+    
+}
+
+struct TabbedView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabbedView()
+    }
+}
