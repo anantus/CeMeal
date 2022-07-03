055650e5939fe2f1b49ff876bc78c4d1f2b38a07
anantus <maheswaraananta@gmail.com>
2022-07-03 21:07:27 +0700

    Adding recipe filter to CeMeal

--- CeMeal/View/GenerateRecipeView/GenerateRecipeView.swift
+++ CeMeal/View/GenerateRecipeView/GenerateRecipeView.swift
@@ -12,26 +12,34 @@ struct GenerateRecipeView: View {
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var recipeViewModel = RecipeViewModel()
     
+    @Environment(\.managedObjectContext) private var viewContent
+    @EnvironmentObject var leftoversViewModel:LeftoversViewModel
+    
+    @FetchRequest(entity: Leftovers.entity(), sortDescriptors: [NSSortDescriptor(key: "dateCreated", ascending: true)]) var fetchedLeftovers:FetchedResults<Leftovers>
+    
 //    @State private var moreIngredients = [String]()
     @State private var moreIngredients = ["Onion", "Garlic", "Chicken", "Apple"]
     
     var body: some View {
+//        List(recipeViewModel.recipes){recipe in
+//            Text(recipe.mealName)
+//        }
+        
         List {
             // MARK: With your ingredients title
-            RecipeSectionView(title: "With your ingredients", subtitle: "You have 7 ingredients", hasActionButton: false)
-            
-            // MARK: With your ingredients recipes
-            ForEach(recipeViewModel.recipes) { recipe in
+            RecipeSectionView(title: "With your ingredients", subtitle: "You have \(generateRecipeBasedOnIngredients().count) ingredients", hasActionButton: false)
+            ForEach(generateRecipeBasedOnIngredients()) { recipe in
                 NavigationLink {
                     RecipeView(recipe: recipe)
                 } label: {
                     RecipeListView(recipe: recipe)
+//                    Text(recipe.ingredients)
                 }
             }
-            
+
             // MARK: With more ingredients title
             RecipeSectionView(title: "With more ingredients", subtitle: nil, hasActionButton: true)
-            
+
             // MARK: More ingredients badges
             if moreIngredients.count > 0 {
                 ScrollView(.horizontal) {
@@ -42,9 +50,9 @@ struct GenerateRecipeView: View {
                     }
                 }
             }
-            
+
             // MARK: With more ingredients recipes
-            ForEach(recipeViewModel.recipes) { recipe in
+            ForEach(generateMoreBasedOnIngredients()) { recipe in
                 NavigationLink {
                     RecipeView(recipe: recipe)
                 } label: {
@@ -53,7 +61,7 @@ struct GenerateRecipeView: View {
             }
         }
         .listStyle(.plain)
-        
+
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarBackButtonHidden(true)
         .toolbar {
@@ -68,6 +76,44 @@ struct GenerateRecipeView: View {
                 }
             }
         }
+    }
+    func generateRecipeBasedOnIngredients() -> [Recipe]{
+        var availableRecipe : [Recipe] = []
+        var checkedIngredients : [String] = []
+        for i in fetchedLeftovers{
+            if i.isChecked{
+                checkedIngredients.append(i.ingredients ?? "Invalid Ingredients")
+            }
+        }
+        for meal in recipeViewModel.recipes{
+            let mealIng = meal.ingredients.filter(){$0 != "Water" || $0 != "Salt" || $0 != "Boiling Water" || $0 != "Cold Water"}
+            if mealIng.sorted() == checkedIngredients.sorted(){
+                availableRecipe.append(meal)
+            }
+        }
+        
+        return availableRecipe
+    }
+    
+    
+    func generateMoreBasedOnIngredients() -> [Recipe]{
+        var availableRecipe : [Recipe] = []
+        var checkedIngredients : [String] = []
+        for i in fetchedLeftovers{
+            if i.isChecked{
+                checkedIngredients.append(i.ingredients ?? "Invalid Ingredients")
+            }
+        }
+        for meal in recipeViewModel.recipes{
+            let listSet = Set(meal.ingredients)
+            let findListSet = Set(checkedIngredients)
+            let allElemsContained = findListSet.isSubset(of: listSet)
+            if allElemsContained{
+                availableRecipe.append(meal)
+            }
+        }
+        
+        return availableRecipe
     }
     
 }
--- CeMeal/ViewModel/RecipeViewModel.swift
+++ CeMeal/ViewModel/RecipeViewModel.swift
@@ -10,47 +10,39 @@ import SwiftUI
 class RecipeViewModel: ObservableObject {
     
     @Environment(\.managedObjectContext) private var viewContent
-    var leftoversViewModel:LeftoversViewModel
+//    var leftoversViewModel:LeftoversViewModel
+    @EnvironmentObject var leftoversViewModel:LeftoversViewModel
+    
+    @FetchRequest(entity: Leftovers.entity(), sortDescriptors: [NSSortDescriptor(key: "dateCreated", ascending: true)]) var fetchedLeftovers:FetchedResults<Leftovers>
     @Published var recipes: [Recipe] = []
     
     init() {
         recipes = loadRecipeCSV(from: "Recipes")
-        leftoversViewModel = LeftoversViewModel()
     }
     
-//    func getRecipes() {
-//        recipes.removeAll()
+//    func availableRecipe() -> [Leftovers] {
+//        var recipeList : [Recipe] = []
+//        var checkedLeftover: [Leftovers] = []
 //
-//        let newRecipes = [
-//            Recipe(title: "Krispy Kreme Donut"),
-//            Recipe(title: "Croatian Bean Stew"),
-//            Recipe(title: "Apple Frangipan Tart")
-//        ]
+//        for leftover in fetchedLeftovers{
+//            if leftover.isChecked{
+//                checkedLeftover.append(leftover)
+//            }
+//        }
+//        for meal in recipes{
+//            //NOTE: bisa dicek lagi nnti
+//            let sorted_ingredient = meal.ingredients.sorted()
 //
-//        recipes.append(contentsOf: newRecipes)
+////            if sorted_ingredient.contains(where: checkedLeftover.contains)
+//////                || sorted_ingredient.contains(where: ["Salt","Water","Cold Water","Boiling Water"].contains)
+////            {
+////                recipeList.append(meal)
+////            }
+//
+//
+//        }
+//       return checkedLeftover
 //    }
-    
-//    func compareLeftoverToRecipe() -> Int {
-//        //TODO: Bikin perbandingan ingredients yang perlu
-//    }
-    
-    func availableRecipe() -> [Recipe] {
-        var recipeList : [Recipe] = []
-        
-        for meal in recipes{
-            //NOTE: bisa dicek lagi nnti
-            let sorted_ingredient = meal.ingredients.sorted()
-            let leftovers = leftoversViewModel.getCheckedLeftovers(context: viewContent)
-            
-            if sorted_ingredient.contains(where: leftovers.contains) ||
-                sorted_ingredient.contains(where: ["Salt","Water","Cold Water","Boiling Water"].contains){
-                recipeList.append(meal)
-            }
-            
-            
-        }
-       return recipeList
-    }
     
 }
 
@@ -75,7 +67,7 @@ func loadRecipeCSV(from csvName : String) -> [Recipe] {
 
     //split the long string into an array of "rows" of data. Each row is a string
     //detect "/n" carriage return, then split
-    var rows = data.components(separatedBy: "\n")
+    var rows = data.components(separatedBy: "$")
 
     //remove header rows
     //count the number of header columns before removing
