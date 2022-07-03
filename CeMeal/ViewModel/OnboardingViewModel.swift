2b9d46a77333cef42006ae4da6bbf04931ee7f71
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-07-03 21:35:47 +0800

    Onboarding images

--- CeMeal/ViewModel/OnboardingViewModel.swift
+++ CeMeal/ViewModel/OnboardingViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Darma Wiryanata on 03/07/22.
 //
 
-import Foundation
+import SwiftUI
 
 class OnboardingViewModel: ObservableObject {
     
@@ -17,9 +17,9 @@ class OnboardingViewModel: ObservableObject {
     
     func getContents() {
         let newContents = [
-            Onboarding(id: 0, title: "Store Your Ingredient", subtitle: "Know recommended storage to maximize ingredient shelf life", image: "house"),
-            Onboarding(id: 1, title: "Don’t Forget Your Ingredients", subtitle: "Every ingredient you add unlocks more recipes", image: "house"),
-            Onboarding(id: 2, title: "Be Reminded", subtitle: "Let us remind you to use your ingredients to reduce waste", image: "house")
+            Onboarding(id: 0, title: "Store Your Ingredient", subtitle: "Know recommended storage to maximize ingredient shelf life", image: Image.ui.onboardingGarlic),
+            Onboarding(id: 1, title: "Don’t Forget Your Ingredients", subtitle: "Every ingredient you add unlocks more recipes", image: Image.ui.onboardingCooking),
+            Onboarding(id: 2, title: "Be Reminded", subtitle: "Let us remind you to use your ingredients to reduce waste", image: Image.ui.onboardingClock)
         ]
         
         contents.append(contentsOf: newContents)
